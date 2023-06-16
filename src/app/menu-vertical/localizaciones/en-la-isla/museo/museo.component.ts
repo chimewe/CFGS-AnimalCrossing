95dfc9543a7ab786f368f7080b9c3a4ede38bc38
Andrea <andreafes74@gmail.com>
2023-06-16 18:41:12 +0200

    cambio algunas imágenes

--- src/app/menu-vertical/localizaciones/en-la-isla/museo/museo.component.ts
+++ src/app/menu-vertical/localizaciones/en-la-isla/museo/museo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-museo',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./museo.component.css']
 })
 export class MuseoComponent {
+  clickedImageSrc: string;
+  isClicked: boolean = false;
 
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  getImageSrc(event: MouseEvent) {
+    const clickedElement = event.target as HTMLImageElement;
+    this.clickedImageSrc = clickedElement.src;
+    var prev:any = document.getElementById("preview");
+    prev.src = this.clickedImageSrc;
+
+    var imageSlide = document.getElementsByClassName("img-slide");
+    for(let i=0; i<imageSlide.length; i++){
+      imageSlide[i].classList.remove("active");
+    }
+
+    const nativeElement = this.el.nativeElement;
+    this.renderer.addClass(nativeElement, 'active');
+  }
 }
